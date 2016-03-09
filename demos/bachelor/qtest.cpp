41b6b28e39e131c536443893ac3132e8ea6bb188
Thimo <thimo.wellner@gmx.de>
2016-03-09 12:41:04 +0100

    int main

--- demos/bachelor/qtest.cpp
+++ demos/bachelor/qtest.cpp
@@ -7,19 +7,21 @@
 #include <time.h>
 
 using namespace seqan;
-
+int main()
+{
 typedef GappedShape<HardwiredShape<1> > TInsideShape; // SHAPE: 11 SPAN: 2 
 
 Dna5String seq = "ACGACTACGCTACGATCGTATATTACGCAATGCA";
-Dna5String read2 = "ACTATACG";
+Dna5String read = "ACTATACG";
 
 Index<Dna5String, IndexQGram<TInsideShape> > qgindex(seq);
 
-Dna5String qgram1 = infixWithLength(read2, 0, 2);
-Dna5String qgram2 = infixWithLength(read2, 0, 4);
+Dna5String qgram1 = infixWithLength(read, 0, 2);
+Dna5String qgram2 = infixWithLength(read, 0, 4);
 
 hash(indexShape(qgindex),begin(qgram1));
 std::cout << countOccurences(qgindex, indexShape(qgindex)) << std::endl;
 
 hash(indexShape(qgindex),begin(qgram2));
 std::cout << countOccurences(qgindex, indexShape(qgindex)) << std::endl;
+}
